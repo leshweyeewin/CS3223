0241c91be495da7fbcd152ce087b9c73f49b399a
wireless90 <wireless-@hotmail.com>
2017-03-29 18:50:08 +0800

    with hash

--- src/qp/operators/BlockNestedJoin.java
+++ src/qp/operators/BlockNestedJoin.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Vector;
 
 import qp.utils.Attribute;
 import qp.utils.Batch;
@@ -17,269 +19,333 @@ public class BlockNestedJoin extends Join{
 
 	int batchsize;  //Number of tuples per out batch
 
-	/** The following fields are useful during execution of
-	 ** the NestedJoin operation
-	 **/
-	int leftindex;     // Index of the join attribute in left table
-	int rightindex;    // Index of the join attribute in right table
+    /** The following fields are useful during execution of
+     ** the NestedJoin operation
+     **/
+    int leftindex;     // Index of the join attribute in left table
+    int rightindex;    // Index of the join attribute in right table
 
-	String rfname;    // The file name where the right table is materialize
+    String rfname;    // The file name where the right table is materialize
 
-	static int filenum=0;   // To get unique filenum for this operation
+    static int filenum=0;   // To get unique filenum for this operation
 
-	Batch outbatch;   // Output buffer
-	Batch leftbatch;  // Buffer for left input stream
-	Batch[] block;
-	Batch rightbatch;  // Buffer for right input stream
-	ObjectInputStream in; // File pointer to the right hand materialized file
+    Batch outbatch;   // Output buffer
+    Batch leftbatch;  // Buffer for left input stream
+    Batch rightbatch;  // Buffer for right input stream
+    ObjectInputStream in; // File pointer to the right hand materialized file
 
-	int lcurs;    // Cursor for left side buffer
-	int rcurs;    // Cursor for right side buffer
-	int pcurs; // page index of the block
-	boolean eosl;  // Whether end of stream (left table) is reached
-	boolean eosr;  // End of stream (right table)
+    int lcurs;    // Cursor for left side buffer
+    int rcurs;    // Cursor for right side buffer
+    int pcurs; // page index of the block
+    boolean eosl;  // Whether end of stream (left table) is reached
+    boolean eosr;  // End of stream (right table)
+    
 
-	public BlockNestedJoin(Join jn){
+    HashMap<Integer,Vector<Tuple>> hashTable;
+    int MaxNumTuplesAllowedInHashTable;
+    int currentNumTuplesInHashTable;
+    
+    public BlockNestedJoin(Join jn){
 		super(jn.getLeft(),jn.getRight(),jn.getCondition(),jn.getOpType());
 		schema = jn.getSchema();
 		jointype = jn.getJoinType();
 		numBuff = jn.getNumBuff();
-	}
+		hashTable = new HashMap<>();
+		currentNumTuplesInHashTable = 0;
+		
+    }
+    
+    /** During open finds the index of the join attributes
+     **  Materializes the right hand side into a file
+     **  Opens the connections
+     **/
 
 
-	/** During open finds the index of the join attributes
-	 **  Materializes the right hand side into a file
-	 **  Opens the connections
-	 **/
 
-
-
-	public boolean open(){
+    public boolean open(){
 
 		/** select number of tuples per batch **/
 		int tuplesize=schema.getTupleSize();
 		batchsize=Batch.getPageSize()/tuplesize;
-
-		block = new Batch[numBuff - 2];
-
+		currentNumTuplesInHashTable = 0;
+		MaxNumTuplesAllowedInHashTable = (numBuff - 2) * batchsize;
+		
 		Attribute leftattr = con.getLhs();
 		Attribute rightattr =(Attribute) con.getRhs();
 		leftindex = left.getSchema().indexOf(leftattr);
 		rightindex = right.getSchema().indexOf(rightattr);
 		Batch rightpage;
 		/** initialize the cursors of input buffers **/
-
+	
 		lcurs = 0; rcurs =0;
 		eosl=false;
 		/** because right stream is to be repetitively scanned
 		 ** if it reached end, we have to start new scan
 		 **/
 		eosr=true;
-
+	
 		/** Right hand side table is to be materialized
 		 ** for the Nested join to perform
 		 **/
-
+	
 		if(!right.open()){
-			return false;
+		    return false;
 		}else{
-			/** If the right operator is not a base table then
-			 ** Materialize the intermediate result from right
-			 ** into a file
-			 **/
-
-			//if(right.getOpType() != OpType.SCAN){
-			filenum++;
-			rfname = "NJtemp-" + String.valueOf(filenum);
-			try{
-				ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(rfname));
-				while( (rightpage = right.next()) != null){
-					out.writeObject(rightpage);
-				}
-				out.close();
-			}catch(IOException io){
-				System.out.println("NestedJoin:writing the temporay file error");
-				return false;
+		    /** If the right operator is not a base table then
+		     ** Materialize the intermediate result from right
+		     ** into a file
+		     **/
+	
+		    //if(right.getOpType() != OpType.SCAN){
+		    filenum++;
+		    rfname = "NJtemp-" + String.valueOf(filenum);
+		    try{
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(rfname));
+			while( (rightpage = right.next()) != null){
+			    out.writeObject(rightpage);
 			}
+			out.close();
+		    }catch(IOException io){
+			System.out.println("NestedJoin:writing the temporay file error");
+			return false;
+		    }
 			//}
-			if(!right.close())
-				return false;
+		    if(!right.close())
+			return false;
 		}
 		if(left.open())
-			return true;
+		    return true;
 		else
-			return false;
-
-	}
-
-
-
-	/** from input buffers selects the tuples satisfying join condition
-	 ** And returns a page of output tuples
-	 **/
+		    return false;
+		
+    }
 
 
-	public Batch next(){
+
+    Vector<Tuple> output = new Vector<>();
+    /** from input buffers selects the tuples satisfying join condition
+     ** And returns a page of output tuples
+     **/
+    public Batch next(){
 		//System.out.print("NestedJoin:--------------------------in next----------------");
 		//Debug.PPrint(con);
 		//System.out.println();
+
+		outbatch = new Batch(batchsize);
+    	while(output.isEmpty() == false)
+    	{
+    		outbatch.add(output.elementAt(0));
+    		output.removeElementAt(0);
+    		if(outbatch.isFull())
+    			return outbatch;
+    	}
+    	
 		int i,j;
 		if(eosl){
 			close();
-			return null;
+		    return null;
 		}
-		outbatch = new Batch(batchsize);
 
 
 		while(!outbatch.isFull())
 		{
-			//pcurs points to block index
-			if(eosr==true)
-			{
-				if(pcurs == 0)
-				{
-					/** new left block is to be fetched**/
-					for(int index = 0; index < numBuff-2; index++)
-					{
-						leftbatch =(Batch) left.next();
-
-						block[index] = leftbatch;
-						if(leftbatch == null)
-							break;
-					}
-
-					if(block[0]==null){
-						eosl=true; 
-						return outbatch; //changed here
-					}
-				}
-
-				/** Whenver a new left block came , we have to start the
+			
+		    if(eosr==true) //Start populating hashTable from scratch
+		    {
+		    	hashTable.clear();
+		    	currentNumTuplesInHashTable = 0;
+		    	
+		    	while(currentNumTuplesInHashTable != MaxNumTuplesAllowedInHashTable)
+		    	{
+		    		
+		    		if(leftbatch != null)
+			    	{
+		    			if(leftbatch.size() == 0){
+		    				leftbatch = null;
+		    			}
+		    			//read remaining tuples from leftbatch if exists
+			    		while(leftbatch != null && currentNumTuplesInHashTable != MaxNumTuplesAllowedInHashTable)
+				    	{
+			    			Tuple tuple = leftbatch.elementAt(lcurs);
+			    			int key = tuple.dataAt(leftindex).hashCode();
+			    			if(hashTable.get(key) == null)
+			    				hashTable.put(key, new Vector<Tuple>());
+			    			
+			    			//put tuple in hashtable
+			    			hashTable.get(key).add(tuple);
+			    			currentNumTuplesInHashTable++;
+			    			lcurs++;
+			    			
+			    			if(lcurs == leftbatch.size()){
+			    				lcurs = 0;
+			    				leftbatch = null;
+			    				break;
+			    			}
+				    	}
+			    	}
+			    	
+			    	//Read block*batchsize to hashTable
+			    	if(leftbatch == null){
+			    		leftbatch = (Batch) left.next();
+			    	}
+			    		
+			    	if(leftbatch == null && currentNumTuplesInHashTable == 0) //nothing left to process
+			    	{
+			    		if(output.isEmpty())
+			    		{
+			    			return outbatch.size() == 0 ? null : outbatch;
+			    		}
+			    			
+			    		while(output.isEmpty() == false)
+			        	{
+			        		outbatch.add(output.elementAt(0));
+			        		output.removeElementAt(0);
+			        		if(outbatch.isFull())
+			        			return outbatch;
+			        	}
+			    	}
+			    	else if(leftbatch == null && currentNumTuplesInHashTable != 0)
+			    	{
+			    		break;
+			    	}
+//			    	else if(leftbatch != null && currentNumTuplesInHashTable == 0)
+//			    	{
+//			    		//Read all from leftbatch to hashtable
+//			    		for(i = 0; i < leftbatch.size(); i++)
+//			    		{
+//			    			Tuple tuple = leftbatch.elementAt(i);
+//			    			int key = tuple.dataAt(leftindex).hashCode();
+//			    			if(hashTable.get(key) == null)
+//			    				hashTable.put(key, new Vector<Tuple>());
+//			    			
+//			    			//put tuple in hashtable
+//			    			hashTable.get(key).add(tuple);
+//			    			currentNumTuplesInHashTable++;
+//			    		}
+//			    		
+//			    		lcurs =  0;
+//			    		leftbatch = null;
+//			    		//Process on hashtable
+//			    	}
+			    	else //if(leftbatch != null && currentNumTuplesInHashTable != 0)
+			    	{
+			    		//Read as much as possible to hashtable
+			    		while(currentNumTuplesInHashTable != MaxNumTuplesAllowedInHashTable)
+			    		{
+			    			if(leftbatch.size() == 0)
+			    				break;
+			    			Tuple tuple = leftbatch.elementAt(lcurs);
+			    			int key = tuple.dataAt(leftindex).hashCode();
+			    			if(hashTable.get(key) == null)
+			    				hashTable.put(key, new Vector<Tuple>());
+			    			
+			    			//put tuple in hashtable
+			    			hashTable.get(key).add(tuple);
+			    			currentNumTuplesInHashTable++;
+			    			lcurs++;
+			    			if(lcurs == leftbatch.size())
+			    			{
+			    				lcurs = 0;
+			    				leftbatch = null;
+			    				break;
+			    			}
+			    				
+			    		}
+			    		
+			    		//Process on hashtable
+			    	}
+		    	}//while(currentNumTuplesInHashTable != MaxNumTuplesAllowedInHashTable)
+		    	
+	
+		    
+				/** Whenver a new hashtable made , we have to start the
 				 ** scanning of entire right table
 				 **/
 				try{
-
-					in = new ObjectInputStream(new FileInputStream(rfname));
-					eosr=false; //ENABLE SCANNING OF RIGHT
+		
+				    in = new ObjectInputStream(new FileInputStream(rfname));
+				    eosr=false; //ENABLE SCANNING OF RIGHT
 				}
 				catch(IOException io)
 				{
-					System.err.println("BlockJoin:error in reading the file");
-					System.exit(1);
+				    System.err.println("BlockJoin:error in reading the file");
+				    System.exit(1);
 				}
-
-			}
-
-			while(eosr==false)
-			{
-
+		
+			 } //if(eosr==true)
+		
+			 while(eosr==false)
+			 {
+		
 				try
 				{
-					if(rcurs==0 && lcurs==0)
-					{ //changed rcurs==0 && lcurs==0
-						rightbatch = (Batch) in.readObject();
-					}
-
-
-					if(pcurs == block.length)
-					{
-						pcurs = 0;
-						lcurs = 0;
-						rcurs = 0;
-						break;
-					}
-
-					leftbatch = block[pcurs];
-					if(leftbatch == null)
-					{
-						pcurs = 0;
-						lcurs = 0;
-						rcurs = 0;
-						eosr = true;
-						break;
-					}
-
-					for(i=lcurs;i<leftbatch.size();i++)
-					{
-						Tuple lefttuple = leftbatch.elementAt(i);
-
-						for(j=rcurs;j<rightbatch.size();j++)
-						{
-
-							Tuple righttuple = rightbatch.elementAt(j);
-
-							if(lefttuple.checkJoin(righttuple,leftindex,rightindex))
-							{
-								Tuple outtuple = lefttuple.joinWith(righttuple);
-
-								//Debug.PPrint(outtuple);
-								//System.out.println();
-								outbatch.add(outtuple);
-								//System.out.println("added tuple");
-								if(outbatch.isFull())
-								{
-									if(i==leftbatch.size()-1 && j==rightbatch.size()-1){//case 1
-										lcurs=0;
-										rcurs=0;
-										//System.out.println("case 1");
-									}else if(i!=leftbatch.size()-1 && j==rightbatch.size()-1){//case 2
-										lcurs = i+1;
-										rcurs = 0;
-										//System.out.println("case 2");
-									}
-									else if(i==leftbatch.size()-1 && j!=rightbatch.size()-1){//case 3
-										lcurs = i;
-										rcurs = j+1;
-										//System.out.println("case 3");
-									}
-									else
-									{
-										lcurs = i;
-										rcurs =j+1;
-									}
-									return outbatch;
-								}
-							}//if joins?
-						}
-						rcurs =0;
-					}
-					lcurs=0;
-
+				    if(rcurs==0)
+				    { //changed rcurs==0 && lcurs==0
+				    	rightbatch = (Batch) in.readObject();
+				    }
+				    
+		 			for(j=rcurs;j<rightbatch.size();j++)
+		 			{
+		 			    
+		 			    Tuple righttuple = rightbatch.elementAt(j);
+		 			    int key = righttuple.dataAt(rightindex).hashCode();
+		 			    
+		 			    if(hashTable.get(key) == null)
+		    				continue;
+		 			    Vector<Tuple> leftTuples = hashTable.get(key);
+		 			    
+		 			    for(int x = 0; x < leftTuples.size(); x++)
+		 			    {
+		 			    	Tuple lefttuple = leftTuples.elementAt(x);
+		 			    	 if(lefttuple.checkJoin(righttuple,leftindex,rightindex))
+				 			 {
+					 				Tuple outtuple = lefttuple.joinWith(righttuple);
+					 				output.add(outtuple);	
+					 				
+				 			 }//if joins?
+		 			    }
+		 			   
+		 			}
+		 			rcurs =0;
+		 			while(output.isEmpty() == false)
+		 	    	{
+		 	    		outbatch.add(output.elementAt(0));
+		 	    		output.removeElementAt(0);
+		 	    		if(outbatch.isFull())
+		 	    			return outbatch;
+		 	    	}
 				}
 				catch(EOFException e)
 				{
-					try{
-						in.close();
-					}catch (IOException io){
-						System.out.println("NestedJoin:Error in temporary file reading");
-					}
-					eosr=true;
-					pcurs++;
-					if(pcurs ==  (numBuff-2))
-						pcurs = 0;
+				    try{
+					in.close();
+				    }catch (IOException io){
+					System.out.println("NestedJoin:Error in temporary file reading");
+				    }
+				    eosr=true;
 				}
 				catch(ClassNotFoundException c){
-					System.out.println("NestedJoin:Some error in deserialization ");
-					System.exit(1);
+				    System.out.println("NestedJoin:Some error in deserialization ");
+				    System.exit(1);
 				}
 				catch(IOException io){
-					System.out.println("NestedJoin:temporary file reading error");
-					System.exit(1);
+				    System.out.println("NestedJoin:temporary file reading error");
+				    System.exit(1);
 				}
-			}//while(eosr==false)
+		    }//while(eosr==false)
 		}//while outbatch is full
-		return outbatch;
-	}
+	return outbatch;
+ }
 
 
 
-	/** Close the operator */
-	public boolean close(){
+    /** Close the operator */
+    public boolean close(){
 
-		File f = new File(rfname);
-		f.delete();
-		return true;
+	File f = new File(rfname);
+	f.delete();
+	return true;
 
-	}
+    }
 
 }
